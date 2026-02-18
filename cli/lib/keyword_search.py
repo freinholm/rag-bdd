e6fa7f3778abf976ce1adfed753b1cce63ddb918
Freinholm <aaron.frein@presidio.edu>
2026-02-18 15:47:11 -0800

    Extend preprocess_text to include tokenization of the string

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -8,15 +8,25 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     preprocessed_query = preprocess_text(query)
     for movie in movies:
         preprocessed_title = preprocess_text(movie["title"])
-        if preprocessed_query in preprocessed_title:
+        
+        for word in preprocessed_query:
+            if word in " ".join(preprocessed_title):
+                results.append(movie)
+                break
+        if len(results) >= limit:
+            break
+
+        '''
+        if preprocessed_query.intersection(preprocessed_title):
             results.append(movie)
             if len(results) >= limit:
                 break
+        '''
     return results
 
 
-def preprocess_text(text: str) -> str:
+def preprocess_text(text: str) -> set:
     text = text.lower()
-    translator = str.maketrans('', '', string.punctuation)
-    text = text.translate(translator)
-    return text
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    textset = set(filter(None, text.split()))
+    return textset
