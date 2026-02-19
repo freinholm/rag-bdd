10a978529d3878128c11902ae44f1dbc619ef83f
Freinholm <aaron.frein@presidio.edu>
2026-02-19 13:57:57 -0800

    Add inverted index & shifted to query from it rather than the direct file.

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,20 +1,90 @@
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
 from nltk.stem import PorterStemmer
+import os
+import pickle
 import string
+from collections import Counter, defaultdict
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CACHE_PATH = os.path.join(PROJECT_ROOT, "cache")
+INDEX_PATH = os.path.join(CACHE_PATH, "index.pkl")
+DOCMAP_PATH = os.path.join(CACHE_PATH, "docmap.pkl")
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.index = defaultdict(set)
+        self.docmap: dict[int, dict] = {}
+        self.index_path = os.path.join(CACHE_PATH, "index.pkl")
+        self.docmap_path = os.path.join(CACHE_PATH, "docmap.pkl")
+        self.tf_path = os.path.join(CACHE_PATH, "term_frequencies.pkl")
+        self.term_frequencies = defaultdict(Counter)
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize_text(text)
+        for token in tokens:
+            if token in self.index:
+                self.index[token].union(set([doc_id]))
+            else:
+                self.index[token] = set([doc_id])
+        self.term_frequencies[doc_id].update(tokens)
+
+    def get_documents(self, term):
+        doc_ids = self.index.get(term, set())
+        return sorted(list(doc_ids))
+    
+    def build(self):
+        movies = load_movies()
+        for movie in movies:
+            self.docmap[movie['id']] = movie
+            self.__add_document(movie['id'], f"{movie['title']} {movie['description']}")
+
+    def load(self):
+        with open(INDEX_PATH, 'rb') as index_file:
+            self.index = pickle.load(index_file)
+        with open(DOCMAP_PATH, 'rb') as docmap_file:
+            self.docmap = pickle.load(docmap_file)
+        with open(self.tf_path, "rb") as f:
+            self.term_frequencies = pickle.load(f)
+
+    def save(self):
+        if not os.path.exists(CACHE_PATH):
+            os.makedirs(CACHE_PATH)
+        with open(INDEX_PATH, "wb") as index_file:
+            pickle.dump(self.index, index_file)
+        with open(DOCMAP_PATH, "wb") as docmap_file:
+            pickle.dump(self.docmap, docmap_file)
+        with open(self.tf_path, "wb") as f:
+            pickle.dump(self.term_frequencies, f)
+
+    def get_tf(self, doc_id: int, term: str) -> int:
+        tokens = tokenize_text(term)
+        if len(tokens) != 1:
+            raise ValueError("term must be a single token")
+        token = tokens[0]
+        return self.term_frequencies[doc_id][token]
+
+
+def build_command() -> None:
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    movies = load_movies()
-    results = []
- 
+    idx = InvertedIndex()
+    idx.load()
     query_tokens = tokenize_text(query)
-
-    for movie in movies:
-        title_tokens = tokenize_text(movie["title"])
-        if has_matching_token(query_tokens, title_tokens):
-            results.append(movie)
+    seen, results = set(), []
+    for query_token in query_tokens:
+        matching_doc_ids = idx.get_documents(query_token)
+        for doc_id in matching_doc_ids:
+            if doc_id in seen:
+                continue
+            seen.add(doc_id)
+            doc = idx.docmap[doc_id]
+            results.append(doc)
             if len(results) >= limit:
-                break
+                return results
 
     return results
 
@@ -61,3 +131,9 @@ def tokenize_text(text: str) -> list[str]:
     stemmed_tokens = stem_tokens(filtered_tokens)
 
     return stemmed_tokens
+
+
+def tf_command(doc_id: int, term: str) -> int:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_tf(doc_id, term)
