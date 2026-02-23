41620f72caffaa8d72e228c85273956e0f93fa6b
freinholm <aaron.frein@presidio.edu>
2026-02-23 11:31:20 -0800

    Implement the semantic search for movies.

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,13 @@
 
 import argparse
 
-from lib.semantic_search import embed_text, verify_embeddings, verify_model
+from lib.semantic_search import (
+    embed_text, 
+    embed_query_text,
+    semantic_search,
+    verify_embeddings, 
+    verify_model
+    )
 
 
 def main() -> None:
@@ -20,6 +26,19 @@ def main() -> None:
         "verify_embeddings", help="Verify embeddings for the movie dataset"
     )
 
+    embedquery_parser = subparsers.add_parser(
+        "embedquery", help="Generate an embedding for single search query"
+    )
+    embedquery_parser.add_argument("query", type=str, help="Query to embed")
+
+    search_parser = subparsers.add_parser(
+        "search", help="Search for movies using semantic search"
+    )
+    search_parser.add_argument("query", type=str, help="Search query")
+    search_parser.add_argument(
+        "--limit", type=int, default=5, help="Number of results to return"
+    )
+
     args = parser.parse_args()
 
     match args.command:
@@ -27,8 +46,12 @@ def main() -> None:
             verify_model()
         case "embed_text":
             embed_text(args.text)
+        case "embedquery":
+            embed_query_text(args.query)
         case "verify_embeddings":
             verify_embeddings()
+        case "search":
+            semantic_search(args.query, args.limit)
         case _:
             parser.print_help()
 
